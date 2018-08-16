6126e018aa6f6aca9ac979d5dde6055aa7ace824
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-08-16 13:28:03 +0300

    changeв js logic

--- main.js
+++ main.js
@@ -1,94 +1,58 @@
 "use strict"
-let films = [
-	{
-		name: "Harry Potter and the Sorcerer\'s Stone", 
-		year: 2001, 
-		link: "https://www.kinopoisk.ru/film/689/",
-		done: true
-	},
-	{
-		name: "Infinity", 
-		year: 1996, 
-		link: "https://www.kinopoisk.ru/film/4145/",
-		done: true
-	},
-	{
-		name: "Ready Player One", 
-		year: 2018, 
-		link: "https://www.kinopoisk.ru/film/538225/",
-		done: true
-	},
-	{
-		name: "X+Y", 
-		year: 2014, 
-		link: "https://www.kinopoisk.ru/film/794247/",
-		done: true
-	},
-	{
-		name: "Pollyanna", 
-		year: 2003, 
-		link: "https://www.kinopoisk.ru/film/pollianna-2003-332348/",
-		done: true
-	} 
-];
 
-let audiobooks = [
-	{
-		name: "Stephen William Hawking: A Briefer History of Time", 
-		year: "", 
-		link: "https://audioknigi.club/hoking-kratkaya-istoriya-vremeni-ot-bolshogo-vzryva-do-chernyh-dyr-bigbag",
-		done: false
-	},
-	{
-		name: "Surely You're Joking, Mr. Feynman!",
-		year: "",  
-		link: "https://book-audio.com/47:feinman-richard-vy-konechno-shutite-mister-feinman",
-		done: true
-	},
-	{
-		name: "Братья Стругацкие: Понедельник начинается в субботу",
-		year: "1965",
-		link: "https://audioknigi.club/strugackie-arkadiy-i-boris-ponedelnik-nachinaetsya-v-subbotu",
-		done: true
-	}
-];
-
-let courses = [
-	{
-		name: "The Complete JavaScript Course: Build a Real-World Project", 
-		year: "", 
-		link: "https://coursehunters.net/course/udemy-complete-javascript-course",
-		done: false
-	}
-];
-
-let books = [
-	{
-		name: "Я. И. Перельман: Математика для любознательных",
-		year: "", 
-		link: "", 
-		done: false
-	}
-];
-
-function output_content (from, to) {
-	let temp = "";
-	for (let i = 0; i < from.length; i++) {
-		let name = from[i].name;
-		let year = from[i].year ? "<span class='year'>" + from[i].year + "</span>" : "";
-		let link = from[i].link ? from[i].link : "#";
-		let done = from[i].done ? "completed" : "not-completed";
-		temp += "<p class='content-element " + done + "'><a href='"+ link + "'>" + name + "</a>" + year + "</p>";
-	}
-	to.innerHTML = temp;
-}
-
-var content_films = document.querySelector(".content-films"),
+let content_films = document.querySelector(".content-films"),
 	content_audiobooks = document.querySelector(".content-audiobooks"),
 	content_courses = document.querySelector(".content-courses"),
 	content_books = document.querySelector(".content-books");
-var content = [[films, content_films], [audiobooks,content_audiobooks], [courses, content_courses], [books, content_books]];
 
-for (let i = 0; i < content.length; i++) {
-	output_content(content[i][0], content[i][1]);
+let data = [];
+
+let OneSectionElement = function(type, name, year, link, done, period) {
+	this.type = type;
+	this.name = name;
+	this.year = year;
+	this.link = link;
+	this.done = done;
+	this.period = period;
 }
+
+data.push(
+	// FILMS
+	new OneSectionElement("films", "Harry Potter and the Sorcerer\'s Stone", 2001, "https://www.kinopoisk.ru/film/689/", true, ["summer", 2018]),
+	new OneSectionElement("films", "Infinity", 1996, "https://www.kinopoisk.ru/film/4145/", true, ["summer", 2018]),
+	new OneSectionElement("films", "Ready Player One", 2018, "https://www.kinopoisk.ru/film/538225/", true, ["summer", 2018]),
+	new OneSectionElement("films", "X+Y", 2014, "https://www.kinopoisk.ru/film/794247/", true, ["summer", 2018]),
+	new OneSectionElement("films", "Pollyanna", 2003, "https://www.kinopoisk.ru/film/pollianna-2003-332348/", true, ["summer", 2018]),
+	// AUDIOBOOKS
+	new OneSectionElement("audiobooks", "Stephen William Hawking: A Briefer History of Time", "", "https://audioknigi.club/hoking-kratkaya-istoriya-vremeni-ot-bolshogo-vzryva-do-chernyh-dyr-bigbag", false, ["summer", 2018]),
+	new OneSectionElement("audiobooks", "Surely You're Joking, Mr. Feynman!", "", "https://book-audio.com/47:feinman-richard-vy-konechno-shutite-mister-feinman", true, ["summer", 2018]),
+	new OneSectionElement("audiobooks", "Братья Стругацкие: Понедельник начинается в субботу", 1965, "https://audioknigi.club/strugackie-arkadiy-i-boris-ponedelnik-nachinaetsya-v-subbotu", true, ["summer", 2018]),
+	// COURSES
+	new OneSectionElement("courses", "The Complete JavaScript Course: Build a Real-World Project", "", "https://coursehunters.net/course/udemy-complete-javascript-course", false, ["summer", 2018]),
+	// BOOKS
+	new OneSectionElement("books", "Я. И. Перельман: Математика для любознательных", "", "", false, ["summer", 2018]),
+	);
+
+
+for (let i = 0; i < data.length; i++) {
+
+	let name = data[i].name;
+	let year = data[i].year ? "<span class='year'>" + data[i].year + "</span>" : "";
+	let link = data[i].link ? data[i].link : "#";
+	let done = data[i].done ? "completed" : "not-completed";
+
+	// Компануем в один параграф 
+	let temp = "<p class='content-element " + done + "'><a href='"+ link + "'>" + name + "</a>" + year + "</p>";
+
+	// Распределение по разделам
+	if (data[i].type == "films") {
+		content_films.innerHTML += temp;
+	} else if (data[i].type == "audiobooks") {
+		content_audiobooks.innerHTML += temp;
+	} else if (data[i].type == "courses") {
+		content_courses.innerHTML += temp;
+	} else {
+		content_books.innerHTML += temp;
+	}
+}
+
